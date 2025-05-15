4b9607ab3c34667855ffac376347b1fd29af4bb9
lpoehls <lpoehls@oklogistics.de>
2025-05-15 12:23:00 +0200

    revise the overlay and added nav bars

--- bolzer/src/predictions.js
+++ bolzer/src/predictions.js
@@ -61,3 +61,21 @@ onAuthStateChanged(auth, (user) => {
     });
   }
 });
+const sidebar = document.getElementById("sidebar");
+const openSidebarBtn = document.getElementById("open-sidebar-btn");
+const closeSidebarBtn = document.getElementById("close-sidebar-btn");
+const mainContent = document.getElementById("main-content");
+const tabelleLink = document.getElementById("tabelle-link");
+
+openSidebarBtn.addEventListener("click", () => {
+  sidebar.classList.add("active");
+  mainContent.classList.add("active");
+});
+
+closeSidebarBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  mainContent.classList.remove("active");
+});
+
+
+

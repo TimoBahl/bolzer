24b32fb4a4d439d66c1893438260dc63aed3d760
TimoBahl <141235391+TimoBahl@users.noreply.github.com>
2025-05-20 16:49:55 +0200

    refactor: add function call the api

---
+++ bolzer/functions/index.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const admin = require("firebase-admin");
+
+// Service Account aus GitHub Secret laden
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bolzer-8d71d-default-rtdb.europe-west1.firebasedatabase.app"
+});
+
+// eslint-disable-next-line require-jsdoc
+async function getSpieltage() {
+  const matchDays = 34;
+
+  for (let spieltag = 1; spieltag <= matchDays; spieltag++) {
+    const url = `https://www.thesportsdb.com/api/v1/json/3/eventsround.php?id=4331&r=${spieltag}&s=2024-2025`;
+
+    try {
+      const response = await axios.get(url);
+      await admin.database().ref(`/spieltag/${spieltag}`).set(response.data);
+      console.log(`Spieltag ${spieltag} gespeichert.`);
+    } catch (error) {
+      console.error(`Fehler bei Spieltag ${spieltag}:`, error.message);
+    }
+  }
+}
+
+getSpieltage();
